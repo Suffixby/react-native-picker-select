a411ee9371a8c9728ea476d57ec1548aef8f1d82
Alexey Khanenya <a.khanenya@tapston.com>
2018-11-07 20:40:05 +0300

    fontSize

--- src/index.js
+++ src/index.js
@@ -318,7 +318,7 @@ export default class RNPickerSelect extends PureComponent {
             <View pointerEvents="box-only" style={style.inputIOSContainer}>
                 <TextInput
                     style={[
-                        fontSize: 24,
+                        { fontSize: 24 },
                         !hideIcon ? { paddingRight: 30 } : {},
                         style.inputIOS,
                         this.getPlaceholderStyle(),
@@ -405,7 +405,7 @@ export default class RNPickerSelect extends PureComponent {
             <View style={[defaultStyles.viewContainer, style.viewContainer]}>
                 <Picker
                     style={[
-                        fontSize: 24,
+                        { fontSize: 24 },
                         hideIcon ? { backgroundColor: 'transparent' } : {},
                         style.inputAndroid,
                         this.getPlaceholderStyle(),
